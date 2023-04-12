aeaacae237a6157145cd8cdd06e837bf7c449199
Jason Sobotka <jsobotka@istation.com>
2023-04-11 20:44:20 -0500

    fix struct types for gcc

--- src/heightmap.h
+++ src/heightmap.h
@@ -12,16 +12,17 @@ namespace Heightmap
     constexpr int32_t mapWidth = 512;
     constexpr int32_t mapHeight = 512;
 
-    using HeightMap_t = struct
+    struct HeightMap_t
     {
         uint8_t data[mapWidth][mapHeight];
     };
 
-    using Coord_t = struct
+    struct Coord_t
     {
         int32_t x;
         int32_t y;
     };
+
 
     [[nodiscard]] int32_t distance(const HeightMap_t& heightMap, const Coord_t& start, const Coord_t& end) noexcept;
 }
